7cd6abb8888461eafcf2324ae34ac69582b581e1
Ricardo Amigo <ricardo.amigo@mclaren.com>
2021-03-10 20:02:59 +0000

    final

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from voting.preferences import random_preferences
 from voting.systems import plurality, majoritarian, procon, ranked
-import pprint
 
 preferences = random_preferences.get_random_preferences(
         10000, 
-        ['Lula', 'Bolso', 'Ciro', 'Marina', 'Amoedo'],
-        [100, 100, 5, 5, 1],
-        [2, 2, 0.5, 0.5, 4]
+        ['Andreia', 'Beatriz', 'Carla', 'Daniela', 'Elisa'],
+        [1, 100, 5, 100, 5],
+        [4, 2, 0.5, 2, 0.5]
     )
-# pprint.pprint(preferences)
+# preferences = random_preferences.get_random_preferences(
+#     10000, 
+#     ['Andreia', 'Beatriz', 'Carla', 'Daniela', 'Elisa', 'F', 'G', 'H', 'I', 'J'],
+#     [1, 100, 5, 100, 5, 0.01, 0.01, 0.01, 0.01, 0.01],
+#     [4, 2, 0.5, 2, 0.5, 1, 1, 1, 1, 1]
+# )
 
+print('Votação normal com primeiro e segundo turno:')
 major_results = majoritarian.majoritarian(preferences)
 print(major_results)
+print('--------------------------------------------')
 
+print('Novo sistema:')
 procon_results = procon.procon(preferences)
 print(procon_results)
+print('--------------------------------------------')
 
+print('Preferências em pares de candidatos:')
 ranked_results = ranked.ranked(preferences)
 print(ranked_results)
+print('--------------------------------------------')
